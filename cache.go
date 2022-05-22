6ce7a8224de9bb0d35303df2eddc2b514d4fcec3
Sergey <xxsmotur@gmail.com>
2022-05-22 17:00:42 +0500

    Update cache

--- cache.go
+++ cache.go
@@ -2,6 +2,20 @@ package cache
 
 type Cache[K comparable, V any] struct {
 	capacity int
+	list List[v]
 	table    map[K]*list.Node[KV[K, V]]
 	evictCb  func(key K, val V)
+}
+
+type KV[K comparable, V any] struct {
+	Key K
+	Val V
+}
+
+func New[K comparable, V any](capacity int) *Cache[K, V] {
+	return &Cache[K, V]{
+		capacity: capacity,
+		lru:      list.List[KV[K, V]]{},
+		table:    make(map[K]*list.Node[KV[K, V]]),
+	}
 }
